df12468fdd577b13c6c37bbb8506ab7a2fe4867e
scriptoProgram <fabianmejia828@gmail.com>
2025-03-06 17:59:51 -0600

    navbar1.1.2

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,12 +1,12 @@
 <?php
-    session_start();
-    require_once './../src/helpers/sessionHelper.php';
+    // session_start();
+    // require_once './../src/helpers/sessionHelper.php';
 
-    if (!SessionHelper::isAuthenticated()) {
-        header("Location: /gimnasio/");
-        exit();
-    }
-    echo "Bienvenido, " . $_SESSION['us_id'] . " (ID: " . $_SESSION['us_id'] . ")";
+    // if (!SessionHelper::isAuthenticated()) {
+    //     header("Location: /gimnasio/");
+    //     exit();
+    // }
+    // echo "Bienvenido, " . $_SESSION['us_id'] . " (ID: " . $_SESSION['us_id'] . ")";
 
 ?>
 <!-- <a href="/gimnasio/src/public/logout.php">Cerrar sesión</a> -->
