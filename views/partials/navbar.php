feb95d226225de94bd2bb6fb4354f45016200308
scriptoProgram <fabianmejia828@gmail.com>
2025-03-09 01:42:40 -0600

    navbar1.7.2

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -14,7 +14,7 @@ function renderNavbar($role) { ?>
             <div class="static flex flex-col">
                 <ul class="grid grid-cols-1 gap-1">
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6'>
                                 <path stroke-linecap='round' stroke-linejoin='round'
                                     d='m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25' />
@@ -23,7 +23,7 @@ function renderNavbar($role) { ?>
                         </a>
                     </li>
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                             </svg>
@@ -31,7 +31,7 @@ function renderNavbar($role) { ?>
                         </a>
                     </li>
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
                             </svg>
@@ -39,7 +39,7 @@ function renderNavbar($role) { ?>
                         </a>
                     </li>
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                             </svg>
@@ -47,7 +47,7 @@ function renderNavbar($role) { ?>
                         </a>
                     </li>
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                             </svg>
@@ -55,7 +55,7 @@ function renderNavbar($role) { ?>
                         </a>
                     </li>
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
                                 <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
                                 <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
@@ -65,7 +65,7 @@ function renderNavbar($role) { ?>
                         </a>
                     </li>
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
                                 <path d="M2 12h1"></path>
                                 <path d="M6 8h-2a1 1 0 0 0 -1 1v6a1 1 0 0 0 1 1h2"></path>
@@ -78,7 +78,7 @@ function renderNavbar($role) { ?>
                         </a>
                     </li>
                     <li>
-                        <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
+                        <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1' href='main.php'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46" />
                             </svg>
@@ -92,7 +92,7 @@ function renderNavbar($role) { ?>
         <nav>
             <ul>
                 <li>
-                    <a class='text-white grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1'
+                    <a class='text-white font-semibold grid auto-cols-[minmax(0,1fr)_minmax(0,4fr)] grid-flow-col gap-1'
                         href='/gimnasio/src/public/logout.php'>
                         <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6'>
                             <path stroke-linecap='round' stroke-linejoin='round' d='M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75' />
@@ -103,9 +103,9 @@ function renderNavbar($role) { ?>
         </nav>
         <hr>
         <div class="grid justify-items-center" id="content-user">
-            <h2 class="text-white">¡Bienvenido!</h2>
-            <img class="w-[4rem] rounded-[50%]" src="../public/images/profile-man.png" alt="#">
-            <p class="capitalize text-white text-center"><?php echo strtolower($_SESSION['name']); ?></p>
+            <h2 class="text-white font-semibold m-[0.3rem]">¡Bienvenido!</h2>
+            <img class="w-[4rem] rounded-[50%] m-[0.3rem]" src="../public/images/profile-man.png" alt="#">
+            <p class="capitalize text-white text-center font-semibold m-[0.3rem]"><?php echo strtolower($_SESSION['name']); ?></p>
         </div>
     </div>
 <?php } ?>
